0718471ef83d463ddebfafbc4d18cda5aefa5880
RicardoQS23 <rpquerido1@gmail.com>
2023-12-28 05:03:35 +0000

    Created base page to display clients in webapp

--- workspace/app/app.py
+++ workspace/app/app.py
@@ -3,13 +3,12 @@
 # Distributed under the terms of the Modified BSD License.
 import os
 from logging.config import dictConfig
-
 import psycopg
 from flask import Flask, flash, jsonify, redirect, render_template, request, url_for
 from psycopg.rows import namedtuple_row
 
 # postgres://{user}:{password}@{hostname}:{port}/{database-name}
-DATABASE_URL = os.environ.get("DATABASE_URL", "postgres://bank:bank@postgres/bank")
+DATABASE_URL = "postgres://clinic:clinic@postgres/clinic" #os.environ.get("DATABASE_URL", "postgres://clinic:clinic@postgres/clinic")
 
 dictConfig(
     {
@@ -44,97 +43,154 @@ def is_decimal(s):
         return False
 
 
+# @app.route("/accounts", methods=("GET",))
+# def account_index():
+#     """Show all the accounts, most recent first."""
+
+#     with psycopg.connect(conninfo=DATABASE_URL) as conn:
+#         with conn.cursor(row_factory=namedtuple_row) as cur:
+#             accounts = cur.execute(
+#                 """
+#                 SELECT account_number, branch_name, balance
+#                 FROM account
+#                 ORDER BY account_number DESC;
+#                 """,
+#                 {},
+#             ).fetchall()
+#             log.debug(f"Found {cur.rowcount} rows.")
+
+#     return render_template("account/index.html", accounts=accounts)
+
+
+# @app.route("/accounts/<account_number>/update", methods=("GET",))
+# def account_update_view(account_number):
+#     """Show the page to update the account balance."""
+
+#     with psycopg.connect(conninfo=DATABASE_URL) as conn:
+#         with conn.cursor(row_factory=namedtuple_row) as cur:
+#             account = cur.execute(
+#                 """
+#                 SELECT account_number, branch_name, balance
+#                 FROM account
+#                 WHERE account_number = %(account_number)s;
+#                 """,
+#                 {"account_number": account_number},
+#             ).fetchone()
+#             log.debug(f"Found {cur.rowcount} rows.")
+
+#     return render_template("account/update.html", account=account)
+
+
+# @app.route("/accounts/<account_number>/update", methods=("POST",))
+# def account_update_save(account_number):
+#     """Update the account balance."""
+
+#     balance = request.form["balance"]
+
+#     error = None
+
+#     if not balance:
+#         error = "Balance is required."
+#     if not is_decimal(balance):
+#         error = "Balance is required to be decimal."
+
+#     if error is not None:
+#         flash(error)
+#     else:
+#         with psycopg.connect(conninfo=DATABASE_URL) as conn:
+#             with conn.cursor(row_factory=namedtuple_row) as cur:
+#                 cur.execute(
+#                     """
+#                     UPDATE account
+#                     SET balance = %(balance)s
+#                     WHERE account_number = %(account_number)s;
+#                     """,
+#                     {"account_number": account_number, "balance": balance},
+#                 )
+#             conn.commit()
+#         return redirect(url_for("account_index"))
+
+
+# @app.route("/accounts/<account_number>/delete", methods=("POST",))
+# def account_delete(account_number):
+#     """Delete the account."""
+
+#     with psycopg.connect(conninfo=DATABASE_URL) as conn:
+#         with conn.cursor(row_factory=namedtuple_row) as cur:
+#             cur.execute(
+#                 """
+#                 DELETE FROM account
+#                 WHERE account_number = %(account_number)s;
+#                 """,
+#                 {"account_number": account_number},
+#             )
+#         conn.commit()
+#     return redirect(url_for("account_index"))
+
+# @app.route("/", methods=["GET", "POST"])
+# @app.route("/login", methods=["GET", "POST"])
+# def login_route():
+#     if request.method == 'POST':
+#         entered_vat_id = request.form.get('client_id')
+#         print(DATABASE_URL, file=sys.stderr)
+#         with psycopg.connect(conninfo=DATABASE_URL) as conn:
+#             with conn.cursor(row_factory=namedtuple_row) as cur:
+#                 cur.execute(
+#                     """
+#                     SELECT vat_id
+#                     FROM client;
+#                     """
+#                 )
+#                 client_vat_ids = cur.fetchall()
+#                 if entered_vat_id in client_vat_ids:
+#                     return redirect(url_for('clients'))
+#                 else:
+#                     return render_template('clinic/index.html', error_message='Invalid VAT ID')
+                
+#     return render_template('clinic/index.html')
+
+# @app.route("/ping", methods=("GET",))
+# def ping():
+#     log.debug("ping!")
+#     return jsonify({"message": "pong!", "status": "success"})
+
 @app.route("/", methods=("GET",))
-@app.route("/accounts", methods=("GET",))
-def account_index():
-    """Show all the accounts, most recent first."""
+@app.route("/clients", methods=("GET",))
+def clients_index():
+    """Show all the clients."""
 
     with psycopg.connect(conninfo=DATABASE_URL) as conn:
         with conn.cursor(row_factory=namedtuple_row) as cur:
-            accounts = cur.execute(
+            clients = cur.execute(
                 """
-                SELECT account_number, branch_name, balance
-                FROM account
-                ORDER BY account_number DESC;
+                SELECT vat_id, name, street
+                FROM client;
                 """,
                 {},
             ).fetchall()
             log.debug(f"Found {cur.rowcount} rows.")
 
-    return render_template("account/index.html", accounts=accounts)
+    return render_template("clinic/index.html", clients=clients)
 
-
-@app.route("/accounts/<account_number>/update", methods=("GET",))
-def account_update_view(account_number):
-    """Show the page to update the account balance."""
+@app.route("/clients/<client_id>/appointments", methods=("GET",))
+def client_appointment_view(client_id):
+    """Show client's appointments and consultations."""
 
     with psycopg.connect(conninfo=DATABASE_URL) as conn:
         with conn.cursor(row_factory=namedtuple_row) as cur:
-            account = cur.execute(
+            appointments = cur.execute(
                 """
-                SELECT account_number, branch_name, balance
-                FROM account
-                WHERE account_number = %(account_number)s;
+                SELECT DISTINCT ap.vat_doctor, ap.date_timestamp, ap.description
+                FROM client AS c
+                JOIN appointment AS ap ON c.vat_id = ap.vat_client
+                JOIN consultation AS co ON ap.VAT_doctor = co.vat_doctor AND ap.date_timestamp = co.date_timestamp
+                WHERE c.vat_id = %(client_id)s;
                 """,
-                {"account_number": account_number},
-            ).fetchone()
+                {"client_id": client_id},
+            ).fetchall()
             log.debug(f"Found {cur.rowcount} rows.")
 
-    return render_template("account/update.html", account=account)
-
-
-@app.route("/accounts/<account_number>/update", methods=("POST",))
-def account_update_save(account_number):
-    """Update the account balance."""
-
-    balance = request.form["balance"]
-
-    error = None
-
-    if not balance:
-        error = "Balance is required."
-    if not is_decimal(balance):
-        error = "Balance is required to be decimal."
-
-    if error is not None:
-        flash(error)
-    else:
-        with psycopg.connect(conninfo=DATABASE_URL) as conn:
-            with conn.cursor(row_factory=namedtuple_row) as cur:
-                cur.execute(
-                    """
-                    UPDATE account
-                    SET balance = %(balance)s
-                    WHERE account_number = %(account_number)s;
-                    """,
-                    {"account_number": account_number, "balance": balance},
-                )
-            conn.commit()
-        return redirect(url_for("account_index"))
-
-
-@app.route("/accounts/<account_number>/delete", methods=("POST",))
-def account_delete(account_number):
-    """Delete the account."""
-
-    with psycopg.connect(conninfo=DATABASE_URL) as conn:
-        with conn.cursor(row_factory=namedtuple_row) as cur:
-            cur.execute(
-                """
-                DELETE FROM account
-                WHERE account_number = %(account_number)s;
-                """,
-                {"account_number": account_number},
-            )
-        conn.commit()
-    return redirect(url_for("account_index"))
-
-
-@app.route("/ping", methods=("GET",))
-def ping():
-    log.debug("ping!")
-    return jsonify({"message": "pong!", "status": "success"})
-
+    return render_template("clinic/appointment.html", appointments=appointments)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
